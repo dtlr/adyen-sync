ad66ac8bbc72017887b8cdf422eb06ad51930d77
Nigel Williams-Lucas <4689066+nwlucas@users.noreply.github.com>
2024-12-09 00:56:36 -0500

    added esbuild

--- bin/dev.js
+++ bin/dev.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env -S node --loader ts-node/esm --disable-warning=ExperimentalWarning
 
-// eslint-disable-next-line n/shebang
-import {execute} from '@oclif/core'
-
-await execute({development: true, dir: import.meta.url})
+// eslint-disable-next-line node/shebang
+;(async () => {
+  const oclif = await import('@oclif/core')
+  await oclif.execute({ development: true, dir: import.meta.url })
+})()
