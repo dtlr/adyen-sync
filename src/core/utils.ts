4def4fd6b2810fbd3909ea0583c882d43f143bd9
Nigel Williams-Lucas <4689066+nwlucas@users.noreply.github.com>
2024-12-10 20:33:03 -0500

    Removed pretty print from logger

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -8,12 +8,7 @@ export const createLogger = (context?: string) =>
     level: (process.env.LOG_LEVEL as string)?.toLowerCase() || 'info',
     defaultMeta: { context: context || 'jdna-sync' },
     transports: [new transports.Console({ forceConsole: true })],
-    format: format.combine(
-      format.timestamp(),
-      format.errors({ stack: true }),
-      format.json(),
-      format.prettyPrint(),
-    ),
+    format: format.combine(format.timestamp(), format.errors({ stack: true }), format.json()),
   })
 
 export const logger = (context?: string) => {
