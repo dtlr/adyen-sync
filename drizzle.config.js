ae0600f02e686e41f4264711663b45002eb56ade
Nigel Williams-Lucas <4689066+nwlucas@users.noreply.github.com>
2024-12-13 10:35:11 -0500

    Updating scripts

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,18 +1,12 @@
 import 'dotenv/config'
 import { defineConfig } from 'drizzle-kit'
 
-const { APP_NEON_DATABASE_URI } = process.env
-
-if (!APP_NEON_DATABASE_URI) {
-  throw new Error('APP_NEON_DATABASE_URI is not set')
-}
-
 export default defineConfig({
   out: './drizzle',
   schema: './src/db/neonSchema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: APP_NEON_DATABASE_URI,
+    url: process.env.APP_NEON_DATABASE_URI,
   },
   introspect: {
     casing: 'camel',
